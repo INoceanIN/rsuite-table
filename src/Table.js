68c374ab0a54957da729f9905fc7552fcabd4d87
ocean <1740664560@qq.com>
2018-12-12 16:56:11 +0800

    22

--- src/Table.js
+++ src/Table.js
@@ -838,7 +838,7 @@ class Table extends React.Component<Props, State> {
         if (shouldRenderExpandedRow) {
           let expand=this.tableRows[index].querySelector(`.${this.addPrefix('row-expanded')}`);
           console.log('expand',expand);
-          nextRowHeight += expand? expand.getHeight(cell): 0;
+          nextRowHeight += expand? expand.getHeight(expand): 0;
         }
 
         /**
